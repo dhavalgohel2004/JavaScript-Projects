d905dc9b9d684ba2c22f223628ca9d6f65a3ac6d
dhaval <dgohel399@gmail.com>
2025-01-07 15:26:11 +0530

    new file added

---
+++ Strings.js
@@ -0,0 +1,21 @@
+const name = "dhaval";
+const age = 20;
+
+// console.log(name + " " + age + " value")
+
+console.log(`helli my name is ${name.toUpperCase()} and my age is ${age}`); 
+
+const StringEx = new String('dhavalGohel')
+
+console.log(StringEx.charAt(5))
+console.log(StringEx.at('h'))
+console.log(StringEx.substring(5))
+// console.log(StringEx.charAt(5))
+
+const url = "https://www.youtube.com/20/-";
+
+console.log(url.includes('youtube'))
+
+const trimString = "    hello   ";
+
+console.log(trimString.trim())
