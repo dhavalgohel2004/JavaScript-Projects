fb1cca4e7e194e9911ae93861c2f65c2f2f1c1fe
dhaval <dgohel399@gmail.com>
2025-01-07 12:37:01 +0530

    new file added of variables

---
+++ Variables.js
@@ -0,0 +1,17 @@
+simpleVariable = "this is simple variable";
+
+let letVariable = "this is let variable";
+
+var varVariable = "this is var variable";
+
+const constVariable = "this is const variable";
+
+simpleVariable = "this is simple variable changed";
+letVariable = "this is let variable changed";
+var varVariable = "this is var variable changed";
+// constVariable = "this is const variable changed";
+
+console.log(simpleVariable);
+console.log(letVariable);
+console.log(varVariable);
+console.log(constVariable);
